b7df453a9dc6eeb526bcda56b02a9d9463aac56f
dv0c <tasosmeidanis12@gmail.com>
2023-04-29 17:13:57 +0300

    new file:   .env 	new file:   .vscode/settings.json 	new file:   app/Login/Form.tsx 	new file:   app/api/register/route.ts 	new file:   app/components/ui/button.tsx 	new file:   app/components/ui/input.tsx 	modified:   app/globals.css 	modified:   app/layout.tsx 	new file:   app/libs/prismadb.ts 	modified:   app/page.tsx 	new file:   global.d.ts 	new file:   lib/utils.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   prisma/schema.prisma 	modified:   tailwind.config.js

---
+++ app/Login/Form.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Input } from "../components/ui/input"
+
+type Props = {}
+
+export const Form = (props: Props) => {
+  return (
+    <div>
+        <div className=''>
+            <button className='border p-3 rounded-md'>
+              Google
+            </button>
+        </div>
+    </div>
+  )
+}
